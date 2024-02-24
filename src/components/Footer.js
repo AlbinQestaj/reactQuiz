11c585b3a8c82cf7c990c345cc8f03ed9856f46e
AlbinQestaj <albinqestaj@gmail.com>
2024-02-24 17:01:59 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import react from 'react'
+
+const Footer = ({children}) => {
+  return (
+    <footer>{children}</footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Question.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Question({ question, dispatch, answer }) {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <div className="options">
+        {question.options.map((option, index) => (
+          <button
+            className={`btn btn-option ${index === answer ? "answer" : ""} 
+            ${
+              answer !== null
+                ? index === question.correctOption
+                  ? "correct"
+                  : "wrong"
+                : ""
+            }`}
+            onClick={() => dispatch({ type: "newAnswer", payload: index })}
+            key={option}
+            disabled={answer !== null}
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
